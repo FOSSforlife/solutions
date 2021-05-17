30cd75375b0ce3ae49630d67fef7f6f7684be0fd
Elias Jackson <ejthedj0@gmail.com>
2021-05-16 18:54:19 -0700

    Fix Medium updater script so it knows when to trigger github actions

--- scripts/server-schedule.js
+++ scripts/server-schedule.js
@@ -7,32 +7,28 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const jobs = [
-  schedule.scheduleJob('*/30 * * * *', async function () {
+  schedule.scheduleJob('*/30 * * * *', async function(){
     const MEDIUM_LINK = 'https://medium.com/feed/solutions';
-    const WEBHOOK_URL =
-      'https://api.github.com/repos/FOSSforlife/solutions/dispatches';
+    const WEBHOOK_URL = 'https://api.github.com/repos/FOSSforlife/solutions/dispatches';
     const SOLUTIONS_FEED_FILE = 'data/solns-medium.json';
 
     console.log('Fetching Medium RSS');
     const feed = await parser.parseURL(MEDIUM_LINK);
-    const cachedFeed = fs.readFileSync(SOLUTIONS_FEED_FILE, { flag: 'a+' });
+    const cachedFeed = JSON.parse(fs.readFileSync(SOLUTIONS_FEED_FILE, {flag: 'a+'}));
 
-    if (JSON.stringify(feed) !== JSON.stringify(JSON.parse(cachedFeed))) {
+    if(feed.items[0]['content:encoded'] !== cachedFeed.items[0]['content:encoded']) {
       console.log('Content has changed. Triggering webhook');
-      await axios.post(
-        WEBHOOK_URL,
-        {
-          event_type: 'fetch-show-notes',
-        },
-        {
-          headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: `Bearer ${process.env.GITHUB_REPOSITORY_DISPATCH_TOKEN}`,
-            'Content-Type': 'application/javascript',
-          },
+      await axios.post(WEBHOOK_URL, {
+        event_type: 'fetch-show-notes'
+      }, {
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          Authorization: `Bearer ${process.env.GITHUB_REPOSITORY_DISPATCH_TOKEN}`,
+          'Content-Type': 'application/javascript'
         }
-      );
+      });
+      console.log(`Writing to ${SOLUTIONS_FEED_FILE}\n`);
       fs.writeFileSync(SOLUTIONS_FEED_FILE, JSON.stringify(feed));
     }
-  }),
+  })
 ];
